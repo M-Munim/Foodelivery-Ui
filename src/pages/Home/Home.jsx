eb7d52755577eae72f2035d524b3eabbcfdd6313
munim <muhammadmunimoff330@gmail.com>
2024-05-31 11:26:10 +0500

    initial UI,  header and Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <>
+      <div>Home</div>
+      <Header />
+    </>
+  )
+}
+
+export default Home
