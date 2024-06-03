53756cfe9cc213ed793ae383383d97ceb79d96a3
munim <muhammadmunimoff330@gmail.com>
2024-06-03 11:33:14 +0500

    food display component using useContext

--- src/components/context/StoreContext.jsx
+++ src/components/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../../assets/assets";
 
 
@@ -9,8 +9,32 @@ export const StoreContext = createContext(null)
 // This part defines a React component named StoreContextProvider.
 // Inside this component, an empty object { } is assigned to contextValue. Which will hold the data you want to share across components. The StoreContextProvider component returns a StoreContext.Provider component.
 const StoreContextProvider = (props) => {
+
+  const [cartItems, setCartItems] = useState({})
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    }
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+  }
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems])
+
+
   const contextValue = {
-    food_list
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
   }
   return (
 
